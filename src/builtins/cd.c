b63a2e810742ff94f1913186d26d81944aaf429c
Han Min Thant <hthant@c2r1s1.42singapore.sg>
2024-12-11 17:19:16 +0800

    cd need to test

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: hthant <hthant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 01:44:24 by messs             #+#    #+#             */
-/*   Updated: 2024/12/11 14:21:06 by hthant           ###   ########.fr       */
+/*   Updated: 2024/12/11 17:18:11 by hthant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*find_env_variable(t_env *env_list, const char *variable, size_t len)
 {
 	while (env_list)
 	{
-		if (ft_strncmp(env_list->value, variable, len) == 0)
+		if (env_list->value && ft_strncmp(env_list->value, variable, len) == 0)
 			return (ft_strdup(env_list->value + len));
 		env_list = env_list->next;
 	}
@@ -39,20 +39,29 @@ char	*find_env_variable(t_env *env_list, const char *variable, size_t len)
 int	update_previous_directory(t_env *env_list)
 {
 	char	current_directory[PATH_MAX];
-	char	*previous_directory;
+	char	*new_value;
+	t_env	*oldpwd_node;
 
 	if (!getcwd(current_directory, PATH_MAX))
 		return (ERROR);
-	previous_directory = malloc(ft_strlen("OLDPWD=")
-			+ ft_strlen(current_directory) + 1);
-	if (!previous_directory)
-		return (ERROR);
-	ft_memcpy(previous_directory, "OLDPWD=", 7);
-	ft_memcpy(previous_directory + 7, current_directory,
-		ft_strlen(current_directory) + 1);
-	free_env(env_list);
-	env_init((t_minishell *)&env_list, &previous_directory);
-	free(previous_directory);
+	oldpwd_node = env_list;
+	while (oldpwd_node)
+	{
+		if (ft_strncmp(oldpwd_node->value, "OLDPWD=", 7) == 0)
+		{
+			free(oldpwd_node->value);
+			new_value = malloc(ft_strlen("OLDPWD=")
+					+ ft_strlen(current_directory) + 1);
+			if (!new_value)
+				return (ERROR);
+			ft_memcpy(new_value, "OLDPWD=", 7);
+			ft_memcpy(new_value + 7, current_directory,
+				ft_strlen(current_directory) + 1);
+			oldpwd_node->value = new_value;
+			return (SUCCESS);
+		}
+		oldpwd_node = oldpwd_node->next;
+	}
 	return (SUCCESS);
 }
 
