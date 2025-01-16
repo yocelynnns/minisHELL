a247dca3b59625bd3e3238271f173198ad31e882
yocelynnns <ytheonas05@gmail.com>
2025-01-17 00:03:14 +0800

    DONEE AA

--- src/parsing/token.c
+++ src/parsing/token.c
@@ -6,7 +6,7 @@
 /*   By: yocelynnns <yocelynnns@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:19:25 by ysetiawa          #+#    #+#             */
-/*   Updated: 2025/01/15 01:19:05 by yocelynnns       ###   ########.fr       */
+/*   Updated: 2025/01/16 23:58:30 by yocelynnns       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,248 +49,6 @@ void	add_token(t_token **head, t_token *new_token)
 	}
 }
 
-// char    *ft_strcjoin(char *str, char c)
-// {
-//     char    *result;
-//     int     i;
-//     result = malloc(sizeof(char) * (ft_strlen(str) + 2));
-//     i = 0;
-//     while (str && str[i])
-//     {
-//         result[i] = str[i];
-//         i++;
-//     }
-//     result[i++] = c;
-//     result[i] = '\0';
-//     free(str);
-//     return (result);
-// }
-
-// char    *first_processing(char *str, t_minishell *mini)
-// {
-//     char    *result;
-//     int     i;
-//     int     in_quote;
-//     char    *start;
-//     i = 0;
-//     in_quote = 0;
-//     result = NULL;
-//     while (str[i])
-//     {
-//         if (str[i] == '\\' && !in_quote)
-//         {
-//             if (str[i + 1] == '\\' || str[i + 1] == '\'' 
-//             || str[i + 1] == '$' || str[i + 1] == '\"')
-//             {
-//                 result = ft_strcjoin(result, str[++i]);
-//                 i++;
-//             }
-//             else if (str[i + 1] == '\n')
-//                 i += 2;
-//             else
-//                 result = ft_strcjoin(result, str[i++]);
-//         }
-//         else if (str[i] == '$') {
-// 			start = ++i + str;
-// 			while (str[i] && !ft_strchr("\\\"\' $", str[i]))
-// 				i++;
-// 			start = ft_strndup(start, (str + i) - start);
-// 			char *env_value = get_env_value(start, mini->env);
-// 			if (env_value) {
-// 				result = ft_strjoin(result, env_value);
-// 			} else {
-// 				result = ft_strcjoin(result, '\0'); // Append empty if undefined
-// 			}
-// 			free(start);
-// 		}
-//         else if (str[i] == '\'')
-//         {
-//             i++;
-//             while (str[i] && str[i] != '\'')
-//                 result = ft_strcjoin(result, str[i++]);
-//             i++;
-//         }
-//         else if (str[i] == '\"')
-//         {
-//             in_quote = str[i++];
-//             while (str[i] && in_quote)
-//             {
-//                 if (str[i] == '\\')
-//                 {
-//                     if (str[i + 1] == '\\' || 
-//                     str[i + 1] == '$' || str[i + 1] == '\"')
-//                     {
-//                         result = ft_strcjoin(result, str[++i]);
-//                         i++;
-//                     }
-//                     else if (str[i + 1] == '\n')
-//                         i += 2;
-//                     else
-//                         result = ft_strcjoin(result, str[i++]);
-//                 }
-//                 else if (str[i] == '$')
-//                 {
-//                     start = ++i + str;
-//                     while (str[i] && !ft_strchr("\\\"$", str[i]))
-//                         i++;
-//                     start = ft_strndup(start, (str + i) - start);
-//                     if (start && *start)
-//                     {
-//                         char *env_val = get_env_value(start, mini->env);
-//                         if(env_val == NULL)
-//                             env_val = ""; 
-//                         result = ft_strjoin(result, env_val);
-//                         free(start);
-//                     }
-//                     else
-//                         result = ft_strcjoin(result, str[i - 1]);
-//                 }
-//                 else
-//                 {
-//                     while (str[i] && str[i] != '\"')
-//                         result = ft_strcjoin(result, str[i++]);
-//                     i++;
-//                 }
-//                 if (str[i] == in_quote)
-//                 {
-//                     in_quote = 0;
-//                     i++;
-//                 }
-//             }
-//         }
-//         else
-//             result = ft_strcjoin(result, str[i++]);
-//     }
-//     return (result);
-// }
-
-// t_token *lexer(const char *input, t_minishell *mini)
-// {
-//     t_token *token_list = NULL;
-//     int i = 0;
-//     int start = 0;
-//     char quote = 0; // to track quotes
-//     int last_token_was_pipe = 0; // to track double pipes
-// 	char *status_str;
-	
-//     while (input[i])
-// 	{
-// 		if (input[i] == '$' && (input[i - 1] != '=') && (input[i
-// 						- 1] != '\'') && (input[i - 2] != '='))
-// 		{
-// 			if (input[i + 1] == '?')
-// 			{
-// 				status_str = ft_itoa(g_exit_status);
-// 				add_token(&token_list, create_token(WORD, status_str));
-// 				free(status_str);
-// 				i++;
-// 				start = i + 1;
-// 			}
-// 			else if (isalpha(input[i + 1])) {
-//                 // Handle environment variable, e.g., $USER
-//                 int var_start = i + 1;
-//                 while (input[i + 1] && (isalnum(input[i + 1]) || input[i + 1] == '_')) {
-//                     i++;
-//                 }
-//                 char *var_name = ft_strndup(input + var_start, i + 1 - var_start);
-//                 char *var_value = get_env_value(var_name, mini->env);
-//                 free(var_name);
-
-//                 // Add token with the environment variable's value
-//                 if (var_value) {
-//                     add_token(&token_list, create_token(WORD, var_value));
-//                     // free(var_value);
-//                 } else {
-//                     // If no value found, just add the token as is
-//                     add_token(&token_list, create_token(WORD, ""));
-//                 }
-//                 start = i + 1; // Set start to the next character after the variable name
-//             }
-// 		}
-//         else if (input[i] == '\'' || input[i] == '"')
-//          {
-//             if (quote == 0)
-//                 quote = input[i]; // first quote
-//             else if (quote == input[i])
-//                 quote = 0; // close quote
-//         }
-//         else if (isspace(input[i]) && !quote) {
-//             if (i > start) {
-//                 char *raw_token = strndup(input + start, i - start);
-//                 char *processed_token = first_processing(raw_token, mini);
-//                 add_token(&token_list, create_token(WORD, processed_token));
-//                 free(raw_token);
-//             }
-//             start = i + 1; // move to the next token
-//         }
-//         else if (input[i] == '<' && !quote) {
-//             if (i > start) {
-//                 char *raw_token = strndup(input + start, i - start);
-//                 char *processed_token = first_processing(raw_token, mini);
-//                 add_token(&token_list, create_token(WORD, processed_token));
-//                 free(raw_token);
-//             }
-//             if (input[i + 1] == '<') {
-//                 add_token(&token_list, create_token(HEREDOC, "<<"));
-//                 i++; // skip the second <
-//             } else {
-//                 add_token(&token_list, create_token(REDIRECT_IN, "<"));
-//             }
-//             start = i + 1;
-//         }
-//         else if (input[i] == '>' && !quote) {
-//             if (i > start) {
-//                 char *raw_token = strndup(input + start, i - start);
-//                 char *processed_token = first_processing(raw_token, mini);
-//                 add_token(&token_list, create_token(WORD, processed_token));
-//                 free(raw_token);
-//             }
-//             if (input[i + 1] == '>') {
-//                 add_token(&token_list, create_token(APPEND, ">>"));
-//                 i++;
-//             } else {
-//                 add_token(&token_list, create_token(REDIRECT_OUT, ">"));
-//             }
-//             start = i + 1;
-//         }
-//         else if (input[i] == '|' && !quote) {
-//             if (last_token_was_pipe) {
-//                 fprintf(stderr, "Error: Invalid sequence of consecutive '|' operators\n");
-//                 free_tokens(token_list);
-//                 return NULL;
-//             }
-//             last_token_was_pipe = 1; // set flag for pipe
-//             if (i > start) {
-//                 char *raw_token = strndup(input + start, i - start);
-//                 char *processed_token = first_processing(raw_token, mini);
-//                 add_token(&token_list, create_token(WORD, processed_token));
-//                 free(raw_token);
-//             }
-//             add_token(&token_list, create_token(PIPE, "|"));
-//             start = i + 1;
-//         }
-//         else {
-//             last_token_was_pipe = 0; // reset flag if not a pipe
-//         }
-//         i++;
-//     }
-
-//     if (quote) { // if quote != 0, means unclosed
-//         fprintf(stderr, "Error: Unclosed quote '%c'\n", quote);
-//         free_tokens(token_list);
-//         return NULL;
-//     }
-
-//     if (i > start) {
-//         char *raw_token = strndup(input + start, i - start);
-//         char *processed_token = first_processing(raw_token, mini);
-//         add_token(&token_list, create_token(WORD, processed_token));
-//         free(raw_token);
-//     }
-
-//     return token_list;
-// }
-
 t_ast_node	*build_ast(t_token *tokens)
 {
 	return (parse_pipeline(&tokens));
@@ -305,17 +63,18 @@ void init_lexstate(t_lexer_state *state)
 	state->token_list = NULL;
 }
 
-void checkquote(t_lexer_state *state)
+int checkquote(t_lexer_state *state)
 {
 	if (state->quote)
 	{
 		printf("Error: Unclosed quote '%c'\n", state->quote);
 		free_tokens(state->token_list);
-        return ;
+        return (1);
 	}
+    return (0);
 }
 
-void handle_dollar(const char *input, t_lexer_state *state, t_minishell *mini)
+void handle_dollar(const char *input, t_lexer_state *state)
 {
     if (input[state->i + 1] == '?')
     {
@@ -325,18 +84,11 @@ void handle_dollar(const char *input, t_lexer_state *state, t_minishell *mini)
         state->i++;
         state->start = state->i + 1;
     }
-    else if (isalpha(input[state->i + 1]))
-    {
-        int var_start = state->i + 1;
-        while (isalnum(input[state->i + 1]) || input[state->i + 1] == '_')
-            state->i++;
-        char *var_name = ft_strndup(input + var_start, state->i + 1 - var_start);
-        char *var_value = get_env_value(var_name, mini->env);
-        free(var_name);
-        if (var_value)
-            add_token(&state->token_list, create_token(WORD, var_value));
-        else
-            add_token(&state->token_list, create_token(WORD, ""));
+    else if (input[state->i + 1] == '\0' && input[state->i - 1] != ' ')
+    {    
+        char *empty;
+        empty = "$";
+        add_token(&state->token_list, create_token(WORD, empty));
         state->start = state->i + 1;
     }
 }
@@ -425,17 +177,37 @@ void process_remaining_token(const char *input, t_lexer_state *state, t_minishel
     }
 }
 
+int checkpipe(const char *input, t_lexer_state *state)
+{
+	if (input[state->i] == '|' && input[state->i + 1] != '|')
+	{
+		printf("Error: Syntax error near unexpected token `|'\n");
+		free_tokens(state->token_list);
+        return (1);
+	}
+    else if ((input[state->i] == '"' || input[state->i] == '\'') \
+    && (input[state->i + 1] == '"' || input[state->i + 1] == '\''))
+    {
+        printf("Command not found: \n");
+		free_tokens(state->token_list);
+        return (1);
+    }
+    return (0);
+}
+
 t_token *lexer(const char *input, t_minishell *mini)
 {
     t_lexer_state state;
     
+    int j = 0;
     init_lexstate(&state);
+    checkpipe(input, &state);
     while (input[state.i])
     {
-        if (input[state.i] == '$' && (input[state.i - 1] != '=') && \
-        (input[state.i - 1] != '\'') && (input[state.i - 2] != '='))
-            handle_dollar(input, &state, mini);
-        else if (input[state.i] == '\'' || input[state.i] == '"')
+        if (input[state.i] == '$')
+            handle_dollar(input, &state);
+        else if ((input[state.i] == '\'' || input[state.i] == '"') \
+        && input[state.i - 1] != '\\')
             handle_quotes(input, &state);
         else if (isspace(input[state.i]) && !state.quote)
             handle_spaces(input, &state, mini);
@@ -447,75 +219,13 @@ t_token *lexer(const char *input, t_minishell *mini)
             state.last_token_was_pipe = 0;
         state.i++;
     }
-    checkquote(&state);
+    j = checkquote(&state);
+    if (j)
+        return (NULL);
     process_remaining_token(input, &state, mini);
     return state.token_list;
 }
 
-// void add_raw_token(t_lexer_state *state, const char *input, int *i, t_minishell *mini)
-// {
-//     char *raw_token = strndup(input + state->start, *i - state->start);
-//     char *processed_token = first_processing(raw_token, mini);
-//     add_token(&state->token_list, create_token(WORD, processed_token));
-//     free(raw_token);
-// } 
 
-// void checkpipe(const char *input, int *i, t_lexer_state *state)
-// {
-// 	if (input[*i] == '|')
-// 	{
-// 		printf("Error: Syntax error near unexpected token `|'\n");
-// 		free_tokens(state->token_list);
-// 	}
-// }
-
-// t_token	*lexer(const char *input, t_minishell *mini)
-// {
-// 	t_lexer_state	state;
-// 	int				i;
-
-// 	i = 0;
-// 	init_lexstate(&state);
-// 	checkpipe(input, &i, &state);
-// 	while (input[i])
-// 	{
-// 		if (input[i] == '$' && (input[i - 1] != '=') && (input[i
-// 						- 1] != '\'') && (input[i - 2] != '='))
-// 			handle_variable_expansion(&state, input, &i, mini);
-// 		else if (input[i] == '\'' || input[i] == '"' || isspace(input[i]))
-// 			state.quote = handle_quotes(input[i], state.quote);
-//         else if (isspace(input[i]) && !state.quote)
-//             handle_space(&state, input, &i, mini);
-// 		else if (input[i] == '<' || input[i] == '>' || input[i] == '|')
-// 			handle_special_char(&state, input, &i, mini);
-// 		// else if (input[i] != '\0')
-// 		// 	state.flag = 1;
-// 		i++;
-// 	}
-// 	checkquote(&state);
-// 	if (i > state.start)
-// 		add_raw_token(&state, input, &i, mini);
-// 	return (state.token_list);
-// }
-
-// void handle_expansion(t_lexer_state *state, const char *input, int *i, t_minishell *mini)
-// {
-// 	int var_start;
-// 	char *var_name;
-// 	char *var_value;
-
-// 	if (input[*i - 1] == '\'')
-// 		return ;
-// 	else
-// 	{
-// 		var_start = *i + 1;
-// 		// while (ft_isdigit(input[*i + 1]) || input[*i + 1] == '_')
-//         // 	(*i)++;
-// 		var_name = ft_strndup(input + var_start, *i - var_start + 1);
-// 		var_value = get_env_value(var_name, mini->env);
-// 		if (var_value)
-// 			add_token(&state->token_list, create_token(WORD, ft_strdup(var_value)));
-// 		free(var_name);
-// 	}
-// 	state->start = *i + 1;
-// }
+//   if (input[state.i] == '$' && (input[state.i - 1] != '=') &&
+//         (input[state.i - 1] != '\'') && (input[state.i - 2] != '=') && (input[state.i - 1] != '\\'))
--- src/parsing/token_handlequote.c
+++ src/parsing/token_handlequote.c
@@ -6,7 +6,7 @@
 /*   By: yocelynnns <yocelynnns@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 19:47:30 by ysetiawa          #+#    #+#             */
-/*   Updated: 2025/01/15 01:18:46 by yocelynnns       ###   ########.fr       */
+/*   Updated: 2025/01/17 00:01:51 by yocelynnns       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 typedef struct s_process
 {
-    const char *str;
+    char *str;
     int         i;
     int         in_quote;
     char        *result;
@@ -40,15 +40,15 @@ char    *ft_strcjoin(char *str, char c)
 
 char *handle_backslash(t_process *proc)
 {
-    if (!proc->in_quote && (proc->str[proc->i + 1] == '\\' || 
-                            proc->str[proc->i + 1] == '\'' || 
-                            proc->str[proc->i + 1] == '$' || 
-                            proc->str[proc->i + 1] == '\"'))
+    if ((proc->str[proc->i] == '\\' || 
+                            proc->str[proc->i] == '\'' || 
+                            proc->str[proc->i] == '$' || 
+                            proc->str[proc->i] == '\"'))
     {
         proc->result = ft_strcjoin(proc->result, proc->str[++proc->i]);
         proc->i++;
     }
-    else if (!proc->in_quote && proc->str[proc->i + 1] == '\n')
+    else if (proc->str[proc->i + 1] == '\n')
         proc->i += 2;
     else
         proc->result = ft_strcjoin(proc->result, proc->str[proc->i++]);
@@ -58,48 +58,62 @@ char *handle_backslash(t_process *proc)
 char *handle_single_quote(t_process *proc)
 {
     proc->i++;
-    while (proc->str[proc->i] && proc->str[proc->i] != '\'')
-        proc->result = ft_strcjoin(proc->result, proc->str[proc->i++]);
-    if (proc->str[proc->i] == '\'')
-        proc->i++;
+    while (proc->str[proc->i])
+    {
+        if (proc->str[proc->i] == '\'')
+        {
+            proc->result = ft_strcjoin(proc->result, '\0');
+            proc->i++;
+            break;
+        }
+        else
+            proc->result = ft_strcjoin(proc->result, proc->str[proc->i++]);
+    }
     return proc->result;
 }
 
 char *expand_variable(t_process *proc)
 {
-    const char *start = proc->str + proc->i;
-    while (proc->str[proc->i] && !ft_strchr("\\\"\' $", proc->str[proc->i]))
+    char *start;
+    start = ++proc->i + proc->str;
+    while (proc->str[proc->i] && !ft_strchr("\\\"\'$ ", proc->str[proc->i]))
         proc->i++;
-    char *var_name = ft_strndup(start, proc->str + proc->i - start);
-    char *env_value = get_env_value(var_name, proc->mini->env);
-    free(var_name);
+    start = ft_strndup(start, proc->str + proc->i - start);
+    char *env_value = get_env_value(start, proc->mini->env);
+    if (!proc->result)
+			proc->result = ft_strdup("");
     if (env_value)
         proc->result = ft_strjoin(proc->result, env_value);
     else
-        proc->result = ft_strcjoin(proc->result, '\0'); // Append empty string if undefined
+        proc->result = ft_strcjoin(proc->result, '\0');
+    free(start);
     return proc->result;
 }
 
 char *handle_double_quote(t_process *proc)
 {
-    proc->i++;
-    while (proc->str[proc->i] && proc->str[proc->i] != '\"')
+    proc->in_quote = proc->str[proc->i++];
+    while (proc->str[proc->i] && proc->in_quote)
     {
-        if (proc->str[proc->i] == '\\')
+        if (proc->str[proc->i] == '\\' && proc->str[proc->i + 1] != '\'')
             proc->result = handle_backslash(proc);
-        else if (proc->str[proc->i] == '$')
+        else if (proc->str[proc->i] == '$' && proc->str[proc->i - 1] != '\\')
             proc->result = expand_variable(proc);
+        else if (proc->str[proc->i] == proc->in_quote)
+        {
+            proc->in_quote = 0;
+            proc->result = ft_strcjoin(proc->result, '\0');
+            proc->i++;
+        }
         else
             proc->result = ft_strcjoin(proc->result, proc->str[proc->i++]);
     }
-    if (proc->str[proc->i] == '\"')
-        proc->i++;
     return proc->result;
 }
 
 char *process_character(t_process *proc)
 {
-    if (proc->str[proc->i] == '\\')
+    if (proc->str[proc->i] == '\\' && !proc->in_quote)
         proc->result = handle_backslash(proc);
     else if (proc->str[proc->i] == '$')
         proc->result = expand_variable(proc);
@@ -127,106 +141,3 @@ char *first_processing(char *str, t_minishell *mini)
 
     return proc.result;
 }
-
-// void handle_space(t_lexer_state *state, const char *input, int *i, t_minishell *mini)
-// {
-//     if (*i > state->start)
-//         add_raw_token(state, input, i, mini);
-//     state->start = *i + 1;
-// }
-
-// static int handle_quote_open(t_lexer_state *state, char quote_char)
-// {
-//     if (state->quote == 0)
-//         state->quote = quote_char;
-//     else if (state->quote == quote_char)
-//         state->quote = 0;
-//     else
-//         return (1);
-//     return (0);
-// }
-
-// static char *extract_quoted_content(const char *input, t_lexer_state *state, char quote_char, int *i)
-// {
-//     int temp_index;
-//     char *quoted_content;
-    
-//     temp_index = 0;
-//     quoted_content = malloc(ft_strlen(input) + 1);
-//     while (input[*i] && !(input[*i] == quote_char && state->quote == 0))
-//     {
-//         if (input[*i] == '\\' && input[*i + 1] == quote_char)
-//         {
-//             state->escquo = 1;
-//             quoted_content[temp_index++] = quote_char;
-//             (*i) += 2;
-//         }
-//         else if (input[*i] == quote_char)
-//         {
-//             state->quote = 0;
-//             break ;
-//         }
-//         else
-//             quoted_content[temp_index++] = input[(*i)++];
-//     }
-//     quoted_content[temp_index] = '\0';
-//     return (quoted_content);
-// }
-
-// static char *create_final_token(const char *input, t_lexer_state *state, char *quoted_content, int *i)
-// {
-//     int prefix_len;
-//     char *final_token;
-    
-//     if (!state->escquo)
-//         prefix_len = *i - state->start - ft_strlen(quoted_content) - 1;
-//     else
-//         prefix_len = *i - state->start - ft_strlen(quoted_content) - 3;
-//     final_token = malloc(prefix_len + ft_strlen(quoted_content) + 1);
-//     if (!final_token)
-//     {
-//         free(quoted_content);
-//         return (NULL);
-//     }
-//     ft_strncpy(final_token, (char *)(input + state->start), prefix_len);
-//     final_token[prefix_len] = '\0';
-//     ft_strcat(final_token, quoted_content);
-//     return (final_token);
-// }
-
-// char handle_quotes(char current_quote, char quote)
-// {
-//     if (quote == 0)
-//         return current_quote;
-//     else if (quote == current_quote)
-//         return 0;
-//     return quote;
-// }
-
-// int handle_quotes_spaces(t_lexer_state *state, const char *input, int *i)
-// {
-//     char quote_char;
-//     char *quoted_content;
-//     char *final_token;
-
-//     if (input[*i] == '\'' || input[*i] == '"')
-//     {
-//         quote_char = input[*i];
-//         if (handle_quote_open(state, quote_char) != 0)
-//             return (1);
-//         (*i)++;
-//         quoted_content = extract_quoted_content(input, state, quote_char, i);
-//         if (!quoted_content)
-//             return (1);
-//         final_token = create_final_token(input, state, quoted_content, i);
-//         free(quoted_content);
-//         if (!final_token)
-//             return (1);
-//         add_token(&state->token_list, create_token(WORD, final_token));
-//         free(final_token);
-//         state->start = *i + 1;
-//     }
-//     else if (isspace(input[*i]) && !state->quote)
-//         handle_space(state, input, i);
-//     return (state->quote);
-// }
